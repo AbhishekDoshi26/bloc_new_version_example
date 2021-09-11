d5c91077276e117e5d9aee3ee0d7d7b771b602f4
AbhishekDoshi26 <adoshi26.ad@gmail.com>
2021-09-11 23:36:02 +0530

    added old code too

--- lib/bloc/counter_bloc.dart
+++ lib/bloc/counter_bloc.dart
@@ -1,6 +1,3 @@
-import 'dart:async';
-import 'dart:html';
-
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 
@@ -15,4 +12,19 @@ class CounterBloc extends Bloc<CounterEvent, CounterState> {
         (event, emit) => emit(CounterDataState(counter: (event.counter - 1).toUnsigned(event.counter.bitLength))));
     on<ResetCounterEvent>((event, emit) => emit(CounterDataState(counter: 0)));
   }
+
+  /// This is the old method but you can still use it as it is backward compatible!
+
+  /*@override
+  Stream<CounterState> mapEventToState(CounterEvent event) async* {
+    if (event is InitialCounterEvent) {
+      yield CounterDataState(counter: 0);
+    } else if (event is IncrementCounterEvent) {
+      yield CounterDataState(counter: event.counter + 1);
+    } else if (event is DecrementCounterEvent) {
+      yield CounterDataState(counter: (event.counter - 1).toUnsigned(event.counter.bitLength));
+    } else if (event is ResetCounterEvent) {
+      yield CounterDataState(counter: 0);
+    }
+  }*/
 }
